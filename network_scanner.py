cae7d9256f766d597cd4a7102c7d0f2c9e97c9c1
JinethBosilu <157296244+JinethBosilu@users.noreply.github.com>
2026-02-10 13:32:00 +0530

    made network ip input from user

--- network_scanner.py
+++ network_scanner.py
@@ -5,8 +5,28 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    print("IP\t\t\tMAC Address\n.........................................")
-    for element in answered:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+    # print("IP\t\t\tMAC Address\n.........................................")
+    # for element in answered:
+    #     print(element[1].psrc + "\t\t" + element[1].hwsrc)
 
-scan("172.31.176.0/20")
+    client_list = []
+    for element in answered:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        client_list.append(client_dict)
+    return client_list
+
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n.........................................")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+def get_user_input():
+    ip = input("Enter the IP range to scan: ")
+    if ip == "":
+        print("Please enter a valid IP range.")
+        exit()
+    return ip
+
+user_ip = get_user_input()
+results = scan(user_ip)
+print_result(results)
