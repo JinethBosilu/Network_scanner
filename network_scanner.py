257152e22cc7437418e71984a9fc41982eb00110
JinethBosilu <157296244+JinethBosilu@users.noreply.github.com>
2026-02-10 13:19:31 +0530

    changed the print statement to show the IP and MAC

--- network_scanner.py
+++ network_scanner.py
@@ -4,7 +4,9 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered.summary())
+    answered = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    print("IP\t\t\tMAC Address\n.........................................")
+    for element in answered:
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
 
-scan("192.168.1.1")
+scan("172.31.176.0/20")
